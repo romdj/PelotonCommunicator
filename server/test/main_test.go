4e71c2f222eaab37d16e89af6a21c78e66d44a05
Romain Lussier <romdj_123@hotmail.com>
2022-08-11 18:19:34 -0400

    Added elements for working server side code CI

---
+++ server/test/main_test.go
@@ -0,0 +1,18 @@
+package main
+
+import "testing"
+
+func testEcho(t *testing.T) {
+	// if testing.Short() {
+	t.Skip("skipping test in short mode.")
+	// }
+	// println("123")
+	// t.("Abs(-1) = %d; want 1", got)
+}
+
+// func TestAlbums() {
+
+// 	getAlbums()
+// 	// t.expect
+// 	// Output: [CD1, CD2, (...)]
+// }
